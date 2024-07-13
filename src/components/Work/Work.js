5840c75e69fd13ec81a9ea4c06e67da231d44646
Charbel <btrscharbel@gmail.com>
2024-07-13 21:34:52 +0200

    updated dates

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -7,18 +7,18 @@ function Work() {
             <div className="job">
                 <div className="job-title">Technical Lead - Solution Architect</div>
                 <div className="job-details">Deloitte, Luxembourg</div>
-                <div className="job-duration">March 2023 – Present</div>
+                <div className="job-duration">Mar 2023 – Present</div>
             </div>
 
             <div className="job">
                 <div className="job-title">Technical Lead</div>
                 <div className="job-details">Bottomline Technologies, Kaslik, Lebanon</div>
-                <div className="job-duration">June 2021 – September 2022</div>
+                <div className="job-duration">Jun 2021 – Feb 2022</div>
             </div>
             <div className="job">
                 <div className="job-title">Team Lead / Senior Developer</div>
                 <div className="job-details">Pinpay, Beirut, Lebanon</div>
-                <div className="job-duration">May 2018 – June 2021</div>
+                <div className="job-duration">May 2018 – Jun 2021</div>
             </div>
         </div>
     );
